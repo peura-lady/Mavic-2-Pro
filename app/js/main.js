ab0998bac394a980e9c1a80b43d68835d94a9172
peura-lady <anna.sonninen94@gmail.com>
2020-09-08 21:06:50 +0300

    added CSS to specification and questions section

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,9 @@ $(function(){
         infinite: false,
     });
 
+    $('.questions__item-title').on('click', function(){
+        $('.questions__item').removeClass('questions__item-active');
+        $(this).parent().addClass('questions__item-active');
+    });
   
 });
