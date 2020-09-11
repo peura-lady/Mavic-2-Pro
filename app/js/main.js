1ab44246d24c87d9ece9b5e69aaa83b346d8b3a1
peura-lady <anna.sonninen94@gmail.com>
2020-09-11 23:48:36 +0300

    started adapting for the menu and top page

--- app/js/main.js
+++ app/js/main.js
@@ -10,5 +10,29 @@ $(function(){
         $('.questions__item').removeClass('questions__item-active');
         $(this).parent().addClass('questions__item-active');
     });
+
+    $('#fullpage').fullpage({
+        autoScrolling:true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        menu: '#header__navigation',
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+    $('.menu__list-link').on('click', function(){
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+    });
+
+    $('.logo').on('click', function(){
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+    });
   
 });
